43e4c0b3a0d6a7fd98b3f19e4fa2c2f98603d7c1
HisanaNissar <hisananissar123@gmail.com>
2023-12-06 11:52:01 +0530

    first commit

---
+++ fiel1.py
@@ -0,0 +1,25 @@
+"""f=open("text.txt","w")
+f.write("my first file\n")
+f.close()
+print("file",f.name,"close")"""
+
+'''f1=open("abc.txt","w")
+print("name of the file;",f1.name)
+print("closed or not;",f1.closed)
+print("opening mde:",f1.mode)'''
+       
+'''f=open("text.txt","w")
+f.write("my fiest file\n")
+f.write("this fiest file\n")
+f=open("abc.txt",'r')
+str=f.read()
+print("read string:",str)
+f.close()
+print(f.name,"is closed")'''
+f=open("filei.txt","w")
+seq=["firstline\n","second line\n"]
+f.writeline(seq)
+f.close()
+f=open("file1.txt","r")
+line
+      
